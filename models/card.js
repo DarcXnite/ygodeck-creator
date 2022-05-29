ae241fabe4a2be51ea5c16bdf7a70a49e2781f83
DarcXnite <anhri89@yahoo.com>
2022-05-28 20:38:12 -0500

    models and databse created and migrated

---
+++ models/card.js
@@ -0,0 +1,33 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class card extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      models.card.belongsTo(models.deck);
+    }
+  }
+  card.init(
+    {
+      cardid: DataTypes.INTEGER,
+      name: DataTypes.STRING,
+      type: DataTypes.STRING,
+      desc: DataTypes.STRING,
+      atk: DataTypes.INTEGER,
+      def: DataTypes.INTEGER,
+      race: DataTypes.STRING,
+      attribute: DataTypes.STRING,
+      deckId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "card",
+    }
+  );
+  return card;
+};
---
+++ models/deck.js
@@ -0,0 +1,28 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class deck extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      models.deck.hasMany(models.card);
+      models.deck.belongsTo(models.user);
+    }
+  }
+  deck.init(
+    {
+      deckName: DataTypes.STRING,
+      description: DataTypes.STRING,
+      userId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "deck",
+    }
+  );
+  return deck;
+};
