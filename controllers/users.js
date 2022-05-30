456c1cf14e1969873c4f335ad90233c0e6a98eed
DarcXnite <anhri89@yahoo.com>
2022-05-30 17:23:28 -0500

    finished login and sign up pages and DB

--- controllers/users.js
+++ controllers/users.js
@@ -2,13 +2,41 @@ const express = require("express");
 const router = express.Router();
 const cryptoJS = require("crypto-js");
 const bcrypt = require("bcryptjs");
+const db = require("../models");
+const salts = 12;
 
+// GET // Renders new user page
 router.get("/new", (req, res) => {
-  res.render("users/new");
+  res.render("users/new", { msg: null });
 });
 
+// POST // Create a new user
 router.post("/new", async (req, res) => {
-  res.send("booty booty");
+  try {
+    const input = req.body;
+    const hashedPass = bcrypt.hashSync(input.password, salts);
+    const [user, userCreated] = await db.user.findOrCreate({
+      where: { email: input.email },
+      defaults: { username: input.username, password: hashedPass },
+    });
+
+    if (userCreated) {
+      const encryptId = cryptoJS.AES.encrypt(
+        user.id.toString(),
+        process.env.ENC_KEY
+      ).toString();
+
+      res.cookie("userId", encryptId);
+      res.redirect("/profile");
+    } else {
+      res.render("users/new", {
+        msg: "User already exists, please log in with your username and password",
+      });
+    }
+  } catch (err) {
+    console.warn(err);
+  }
 });
+// end of POST // (╯°□°）╯︵ ┻━┻
 
 module.exports = router;
